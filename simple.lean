4cb991c155b2013c3095bfabb9c74174eca78d0a
fischerman <code@bjorn-fischer.de>
2019-01-10 11:21:40 +0100

    pass equality to get type inference by refl

--- simple.lean
+++ simple.lean
@@ -23,21 +23,14 @@ def type_map : type → Type
 @[reducible]
 def state (sig : signature) : Type := Π n : string, type_map (sig n)
 
-@[reducible]
-def state.update {sig : signature} (name : string) (val : type_map (sig name)) (s : state sig) : state sig := 
-    λ n, 
-    -- if n = name then val else (s n)
-    begin
-        by_cases n = name,
-        {
-            rw h,
-            exact val
-        }, {
-            apply s,
-        }
-    end 
+def state.update {sig : signature} (name : string) (val : type_map (sig name)) (s : state sig) : state sig :=
+λn, if h : n = name then by rw [h]; exact val else (s n)
 
-#print state.update -- val is not of type name, how does eq.mpr work??
+def state.update' {sig : signature} {t : type} {name : string} (eq : sig name = t) (val : type_map t) (s : state sig) : state sig :=
+state.update name (by rw [eq]; exact val) s
+
+def state.get' {sig : signature} {t : type} {name : string} (eq : sig name = t) (s : state sig) : type_map t :=
+by rw [← eq]; exact s name
 
 inductive expression (sig : signature) : type → Type
 | var (n : string) : expression (sig n)
@@ -52,7 +45,10 @@ inductive program (sig : signature)
 | loop (n : string) (h : sig n = int) :
   expression sig int → program → program
 
+infixr ` ;; `:90 := program.seq
+
 open program
+
 
 def eval_expression {sig : signature} (s : state sig) : Π{t : type}, expression sig t → type_map t
 | _ (var sig n) := s n
@@ -63,21 +59,22 @@ def eval_expression {sig : signature} (s : state sig) : Π{t : type}, expression
 def den {sig : signature} : program sig → state sig → state sig
 | (assign var_name indices expr) s := s.update var_name (eval_expression s expr)
 | (seq p₁ p₂) s := den p₂ (den p₁ s)
-| (loop loop_var h expr p) s := nat.iterate (λ s, (den p s).update loop_var (s loop_var + 1)) (eval_expression s expr) (s.update loop_var 0)
+| (loop loop_var h expr p) s' := nat.iterate (λ s, (den p s).update' h (s.get' h)) (eval_expression s' expr) (s'.update' h 0)
 
 
+@[reducible]
 def S1 := (create_signature [("m", int), ("n", int)])
 
 def s : state S1 := sorry
 
 def P1 : program S1 :=
-seq
-    (assign "n" [] (const_int S1 3))
-    (assign "m" [] (add int (const_int S1 39) (var S1 "n")))
+    (assign "n" [] (const_int S1 3)) ;;
+    (assign "m" [] (add int (const_int S1 39) (show expression S1 int, from var S1 "n")))
 
 #reduce den P1 s "m" -- computes 42
 
-example (s : state S1) : den P1 s "m" = 5 := sorry
+example (s : state S1) : (show nat, from den P1 s "m") = 42 := rfl
 
+-- seq p1 p1 = loop n 2 p1
 
 end
