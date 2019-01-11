0e4a8e74e9f4dae9a4c146c33fb14e04dfd88d84
fischerman <code@bjorn-fischer.de>
2019-01-11 14:31:03 +0100

    implement loop_seq lemma

--- simple.lean
+++ simple.lean
@@ -32,10 +32,10 @@ state.update name (by rw [eq]; exact val) s
 def state.get' {sig : signature} {t : type} {name : string} (eq : sig name = t) (s : state sig) : type_map t :=
 by rw [← eq]; exact s name
 
-inductive expression (sig : signature) : type → Type
-| var (n : string) : expression (sig n)
-| add (t : type) : expression t → expression t → expression t
-| const_int : ℕ → expression int
+inductive expression (sig : signature) (t : type) : Type
+| var (n : string) (h : sig n = t) : expression
+| add : expression → expression → expression
+| const_int (n : ℕ) (h : t = int) : expression
 
 open expression
 
@@ -51,15 +51,18 @@ open program
 
 
 def eval_expression {sig : signature} (s : state sig) : Π{t : type}, expression sig t → type_map t
-| _ (var sig n) := s n
-| _ (add int a b) := eval_expression a + eval_expression b
-| _ (add float a b) := eval_expression a + eval_expression b
-| _ (const_int sig a) := a
+| t (var n h) := (by rw [←h]; exact s n)
+| int (add a b) := eval_expression a + eval_expression b
+| float (add a b) := eval_expression a + eval_expression b
+| t (const_int sig n h) := (by rw [h]; exact n)  -- why does this one accept a signature but var does not
+
 
 def den {sig : signature} : program sig → state sig → state sig
 | (assign var_name indices expr) s := s.update var_name (eval_expression s expr)
 | (seq p₁ p₂) s := den p₂ (den p₁ s)
-| (loop loop_var h expr p) s' := nat.iterate (λ s, (den p s).update' h (s.get' h)) (eval_expression s' expr) (s'.update' h 0)
+| (loop loop_var h expr p) s' := nat.iterate (λ s, (den p s).update' h (s.get' h + 1)) (eval_expression s' expr) (s'.update' h 0)
+
+notation `⟦` p `⟧`:= den p
 
 
 @[reducible]
@@ -68,13 +71,150 @@ def S1 := (create_signature [("m", int), ("n", int)])
 def s : state S1 := sorry
 
 def P1 : program S1 :=
-    (assign "n" [] (const_int S1 3)) ;;
-    (assign "m" [] (add int (const_int S1 39) (show expression S1 int, from var S1 "n")))
+    (assign "n" [] (const_int S1 3 (by refl))) ;;
+    (assign "m" [] (add (const_int S1 39 (by refl)) (show expression S1 int, from var "n" (by refl))))
 
 #reduce den P1 s "m" -- computes 42
-
 example (s : state S1) : (show nat, from den P1 s "m") = 42 := rfl
 
+def P2 : program S1 :=
+    (assign "m" [] (const_int S1 0 (by refl))) ;;
+    (loop "n" (by refl) (const_int S1 10 (by refl)) (
+        (assign "m" [] (add (var "m" (by refl)) (var "n" (by refl))))
+    ))
+
+#eval den P2 s "m"
+
+set_option trace.simplify.rewrite true 
+
+example (sig : signature) (p₁ p₂ : program sig) (s : state sig) : ⟦ p₁ ;; p₂ ⟧ s = den p₂ (den p₁ s) := by reflexivity
+
+lemma eval_const_int { sig : signature } { s : state sig } (n : ℕ) : @eval_expression sig s int (const_int sig n (by refl)) = n := begin
+    rw eval_expression,
+    rw eq.mpr,
+end
+
+-- overapproximated
+def modifies {sig : signature} : program sig → set (string)
+| (assign n _ _) := { n }
+| (seq p₁ p₂) := modifies p₁ ∪ modifies p₂
+| (loop n _ _ p) := { n } ∪ modifies p
+
+def expr_reads {sig : signature} {t : type} : expression sig t → set (string) 
+| (var n _) := { n }
+| (add s₁ s₂) := expr_reads s₁ ∪ expr_reads s₂
+| (const_int _ _ _) := { }
+
+#check (@var S1 int "m" (by refl))
+
+#reduce expr_reads (@const_int S1 int 39 (by refl)) -- empty set
+-- sets aren't computable
+--#reduce expr_reads (@var S1 int "m" (by refl)) "m"
+
+example : "m" ∈ expr_reads (@var S1 int "m" (by refl)) := 
+begin
+    rw expr_reads,
+    sorry
+end
+
+-- does not include array accesses yet
+def reads { sig : signature } : program sig → set (string)
+| (assign _ _ expr) := expr_reads expr
+| (seq p₁ p₂) := reads p₁ ∪ reads p₂
+| (loop _ _ expr p) := expr_reads expr ∪ reads p
+
+def uses {sig : signature} : program sig → set (string) 
+| p := modifies p ∪ reads p
+
+-- (a != b) : get a (update b s) = get a s
+-- (a != b) : (update a s) b = s b
+
+lemma state_eliminate_update' (sig : signature) (s : state sig) (a b : string) (t : type) (hat : sig a = t) (hnab : ¬(b = a) ) (val : type_map t) 
+  : (s.update' hat val) b = s b :=
+begin
+    rw state.update',
+    rw state.update,
+    show dite (b = a) (λ (h : b = a), eq.mpr _ (eq.mpr _ val)) (λ (h : ¬b = a), s b) = s b, 
+    rw dite,
+    cases string.has_decidable_eq b a,
+    {
+        refl
+    }, {
+        contradiction
+    }
+end
+
+lemma uses_neq (sig : signature) (p : program sig) (a b : string) (hpua : uses p a) (hpnub : ¬(uses p b)) : ¬(a = b) := begin
+    cases hpua,
+    case or.inl {
+        intro heq,
+        apply hpnub,
+        rw uses,
+        left,
+        rw ← heq,
+        exact hpua
+    },
+    case or.inr {
+        intro heq,
+        apply hpnub,
+        rw uses,
+        right,
+        rw ← heq,
+        exact hpua
+    }
+end
+
+lemma expr_uses_update'_eliminate (sig : signature) (s : state sig) (a : string) (t : type) (hat : sig a = t) (val : type_map t) (expr : expression sig t) (hnu : ¬(expr_reads expr a))
+    : eval_expression (s.update' hat val) expr = eval_expression s expr := begin
+    induction expr,
+    case var {
+        -- how do I unfold eval_expression
+        sorry
+    },
+    sorry,
+    sorry
+end
+
+lemma state_postpone_update' (sig : signature) (s : state sig) (a : string) (t : type) (hat : sig a = t) (val : type_map t) (p : program sig) (hnu : ¬(uses p a)) 
+    : ⟦ p ⟧ (s.update' hat val) = (⟦ p ⟧ s).update' hat val :=
+begin
+    induction p,
+    case assign : b idxs expr {
+        rw den,
+        rw den,
+        have hpub : uses (assign b idxs expr) b := begin
+            rw uses,
+            left,
+            rw modifies,
+            sorry -- how???
+        end,
+        have hneq : ¬(b = a) := begin
+            apply uses_neq sig (assign b idxs expr) b a,
+            repeat { assumption }
+        end,
+        rw ← expr_uses_update'_eliminate,
+    }
+end
+
 -- seq p1 p1 = loop n 2 p1
+lemma loop_seq (sig : signature) (s₁ : state sig) (v : string) (l : string) (hli : sig l = int) 
+        (p : program sig) (hnu : ¬(uses p l)) (hnv : ¬ (v = l)) : ⟦ p ;; p ⟧ s₁ v = ⟦ loop l hli (const_int sig 2 (by refl)) p ⟧ s₁ v :=
+begin
+    rw den,
+    rw eval_const_int,
+    rw nat.iterate,
+    rw nat.iterate,
+    rw nat.iterate,
+    rw den,
+    rw state_eliminate_update',
+    repeat { rw state_postpone_update' },
+    repeat { rw state_eliminate_update' },
+    repeat { assumption }
+end
 
 end
+
+
+-- todo: make sig a parameter
+-- todo: introduce notation
+-- todo: make signature implicit
