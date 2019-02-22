8d85cf577147d4e970a97ebf39ee5e16132664b7
fischerman <code@bjorn-fischer.de>
2019-02-22 15:37:30 +0100

    create exec_memory

--- src/parlang.lean
+++ src/parlang.lean
@@ -124,32 +124,29 @@ end state
 def bool_complement {α : Type} (f: α → bool) : α → bool := λa:α, ¬f a
 
 /-- Execute a kernel on a global state, i.e. a list of threads -/
-inductive exec : kernel σ ι τ → state σ ι τ → state σ ι τ → Prop
+inductive exec_state : kernel σ ι τ → state σ ι τ → state σ ι τ → Prop
 | load (f) (s : state σ ι τ) :
-  exec (load f) s (s.map_active_threads $ thread_state.load f)
+  exec_state (load f) s (s.map_active_threads $ thread_state.load f)
 | store (f) (s : state σ ι τ) :
-  exec (store f) s (s.map_active_threads $ thread_state.store f)
+  exec_state (store f) s (s.map_active_threads $ thread_state.store f)
 | compute (f : σ → σ) (s : state σ ι τ) :
-  exec (compute f) s (s.map_active_threads $ thread_state.map f)
+  exec_state (compute f) s (s.map_active_threads $ thread_state.map f)
 | sync_all (s : state σ ι τ) (m : memory ι τ) (hs : s.syncable m) (ha : s.all_threads_active) :
-  exec sync s (s.map_threads $ thread_state.sync m)
+  exec_state sync s (s.map_threads $ thread_state.sync m)
 | sync_none (s : state σ ι τ) (h : s.no_thread_active) :
-  exec sync s s
+  exec_state sync s s
 | seq (s t u : state σ ι τ) (k₁ k₂ : kernel σ ι τ) :
-  exec k₁ s t → exec k₂ t u → exec (seq k₁ k₂) s u
+  exec_state k₁ s t → exec_state k₂ t u → exec_state (seq k₁ k₂) s u
 | ite (s t u : state σ ι τ) (f : σ → bool) (k₁ k₂ : kernel σ ι τ) :
-  exec k₁ (s.deactive_threads (λl, ¬f l)) t → exec k₂ (t.deactive_threads f) u → exec (ite f k₁ k₂) s u -- in the then-branch we deactive those threads where the condition is false and vice versa
+  exec_state k₁ (s.deactive_threads (λl, ¬f l)) t → exec_state k₂ (t.deactive_threads f) u → exec_state (ite f k₁ k₂) s u -- in the then-branch we deactive those threads where the condition is false and vice versa
 | loop_stop (s : state σ ι τ) (f : σ → bool) (k : kernel σ ι τ) :
-  (∀t∈s.active_threads, ¬f (t:thread_state σ ι τ).tlocal) → exec (loop f k) s s
+  (∀t∈s.active_threads, ¬f (t:thread_state σ ι τ).tlocal) → exec_state (loop f k) s s
 | loop_step (s t u : state σ ι τ) (f : σ → bool) (k : kernel σ ι τ) :
   (∃t∈s.active_threads, f (t:thread_state σ ι τ).tlocal) →
-  exec k (s.deactive_threads (bool_complement f)) t → exec (loop f k) (t.deactive_threads (bool_complement f)) u → exec (loop f k) s (u.mirror_active_threads s) -- IS THIS CORRECT?
+  exec_state k (s.deactive_threads (bool_complement f)) t → exec_state (loop f k) (t.deactive_threads (bool_complement f)) u → exec_state (loop f k) s (u.mirror_active_threads s) -- IS THIS CORRECT?
 
-/-
+inductive exec_memory (k : kernel σ ι τ) (s : state σ ι τ) (m : memory ι τ) : Prop
+| intro (u) (hk : exec_state k s u) (syncable : u.syncable m) : exec_memory
 
-exec' (k : kernel) (i : [σ]) (m : memory) : Prop
-| intro (u) (hk : exec k (create_state i) u) (syncable : u.syncable m) : exec' k i m
-
--/
 
 end parlang
