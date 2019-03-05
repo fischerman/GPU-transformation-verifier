da7a3298d317cb5dbbeaeecdfb2b1ecda3ca3cad
fischerman <code@bjorn-fischer.de>
2019-03-05 11:53:20 +0100

    add incomplete aux lemmas

--- src/aux.lean
+++ src/aux.lean
@@ -2,6 +2,7 @@ import data.list.basic
 import data.vector
 
 universes u v w
+variables {n : ℕ} {α β γ : Type}
 
 namespace list
 
@@ -61,7 +62,54 @@ lemma contains_nth {α : Type} : ∀{n : ℕ} {v : vector α n} {i : fin n}, (v.
     exact ⟨i, hi, rfl⟩
   end
 
-lemma vector_0_eq {α : Type*} : ∀(v : vector α 0), v = vector.nil
+lemma eq_element_wise {α : Type} : ∀{a b : vector α n}, ∀ i, a.nth i = b.nth i → a = b := begin
+  intros a b i hieq,
+  apply vector.eq,
+  cases a,
+  cases b,
+  repeat { rw to_list },
+  simp,
+  induction a_val generalizing b_val,
+  case list.nil {
+    cases b_val,
+    { refl },
+    {
+      rw list.length at a_property b_property,
+      rw ← b_property at a_property,
+      contradiction,
+    }
+  },
+  case list.cons {
+    cases b_val,
+    {
+      rw list.length at a_property b_property,
+      rw ← b_property at a_property,
+      contradiction,
+    },
+    {
+      sorry
+    }
+  }
+end
+-- | ⟨[], _⟩ ⟨[], ha⟩ _ _ := by refl
+-- | ⟨ a :: as , _⟩ ⟨ b :: bs, _⟩ i hieq := begin
+  
+-- end
+
+
+--   intros i hieq,
+--   exact match a with
+--   | ⟨[], ha⟩ := 
+--     begin
+--       exact match b with
+--       | ⟨[], ha⟩ := sorry
+--       |
+--       end,
+--     end
+--   end
+-- end
+
+@[simp] lemma vector_0_eq {α : Type*} : ∀(v : vector α 0), v = vector.nil
 | ⟨l, hl⟩ := subtype.eq $ show l = list.nil, from list.length_eq_zero.1 hl
 
 @[simp] lemma map₂_nil {α β γ : Type} {f : α → β → γ} :
@@ -70,6 +118,10 @@ lemma vector_0_eq {α : Type*} : ∀(v : vector α 0), v = vector.nil
 
 @[simp] lemma map₂_nil' {α β γ : Type} {f : α → β → γ} (v : vector α 0) (p) :
   vector.map₂ f v ⟨list.nil, p⟩ = vector.nil := sorry
+
+@[simp] lemma nth_map (f : α -> β) (v : vector α n) (i) : (v.map f).nth i = f (v.nth i) := sorry
+
+@[simp] lemma nth_map₂ (f : α → β → γ) (v : vector α n) (w : vector β n) (i) : nth (map₂ f v w) i = f (v.nth i) (w.nth i) := sorry
 
 -- example {f a as b bs h h' h''} :
 --   vector.map₂ f ⟨ a :: as, h⟩ ⟨b :: bs, h'⟩ = ⟨f a b :: vector.map₂ f as bs, h''⟩ :=
