8e4ec2d16af0ea33e8315e863299ff1963482443
fischerman <code@bjorn-fischer.de>
2019-03-25 11:55:35 +0100

    define three relational hoare logics

--- src/rel_hoare.lean
+++ src/rel_hoare.lean
@@ -2,12 +2,18 @@ import parlang
 
 namespace parlang
 
-variables {σ : Type} {ι : Type} {τ : ι → Type} [decidable_eq ι]
+variables {σ σ₁ σ₂ : Type} {ι : Type} {τ τ₁ τ₂ : ι → Type} [decidable_eq ι]
 
-def rel_hoare (P : memory τ → memory τ → Prop) (k₁ : kernel σ τ) (k₂ : kernel σ τ) (Q : memory τ → memory τ → Prop) :=
-∀ (n) s u (ac : vector bool n) m m' o o', P m m'  → exec_memory k₁ ac s m o → exec_memory k₂ ac u m' o' → Q o o'
+-- we assume the same type ι for addressing global memory
+def rel_hoare_state (P : Π n₁:ℕ, state n₁ σ₁ τ₁ → vector bool n₁ → Π n₂:ℕ, state n₂ σ₂ τ₂ → vector bool n₂ → Prop) (k₁ : kernel σ₁ τ₁) (k₂ : kernel σ₂ τ₂) 
+    (Q : Π n₁:ℕ, state n₁ σ₁ τ₁ → vector bool n₁ → Π n₂:ℕ, state n₂ σ₂ τ₂ → vector bool n₂ → Prop) : Prop :=
+    ∀ (n₁ n₂ : ℕ) (s₁ s₁' : state n₁ σ₁ τ₁) (s₂ : state n₂ σ₂ τ₂) ac₁ ac₂, P n₁ s₁ ac₁ n₂ s₂ ac₂ → exec_state k₁ ac₁ s₁ s₁' →
+    ∃ s₂', exec_state k₂ ac₂ s₂ s₂' ∧ Q n₁ s₁' ac₁ n₂ s₂' ac₂
 
-notation `{* ` P : 1 ` *} ` k₁ : 1 ` ~ ` k₂ : 1 ` {* ` Q : 1 ` *}` := rel_hoare P k₁ k₂ Q
+def rel_hoare_memory (P : memory τ → memory τ → Prop) (k₁ : kernel σ τ) (k₂ : kernel σ τ) (Q : memory τ → memory τ → Prop) :=
+∀ (n₁ n₂) s (ac₁ : vector bool n₁) (ac₂ : vector bool n₂) m m' o, P m m' → exec_memory k₁ ac₁ s m o → ∃ u o', exec_memory k₂ ac₂ u m' o' → Q o o'
+
+notation `{* ` P : 1 ` *} ` k₁ : 1 ` ~ ` k₂ : 1 ` {* ` Q : 1 ` *}` := rel_hoare_memory P k₁ k₂ Q
 
 namespace rel_hoare
 
@@ -22,5 +28,8 @@ lemma seq {P Q R} {k₁ k₁' k₂ k₂' : kernel σ τ} : {* P *} k₁ ~ k₁' 
 end
 
 end rel_hoare
+
+def rel_hoare_program (init₁ : ℕ → σ₁) (init₂ : ℕ → σ₂) (P : memory τ₁ → memory τ₂ → Prop) (p₁ : program σ₁ τ₁) (p₂ : program σ₂ τ₂) (Q : memory τ₁ → memory τ₂ → Prop) := 
+∀ m₁ m₁' m₂, P m₁ m₂ → exec_prog init₁ p₁ m₁ m₁' → ∃ m₂', exec_prog init₂ p₂ m₂ m₂' ∧ Q m₁' m₂'
 
 end parlang
