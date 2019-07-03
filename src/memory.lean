abab79f4c02dd37fe603cf72b07f2425d0085ef8
fischerman <code@bjorn-fischer.de>
2019-07-03 15:18:00 +0200

    basic properties of memory

---
+++ src/memory.lean
@@ -0,0 +1,19 @@
+import parlang
+
+namespace parlang
+namespace memory
+
+variables {ι : Type} {τ : ι → Type} [decidable_eq ι] {m : memory τ} {i i' : ι} {val : τ i}
+
+lemma get_update_success : memory.get (memory.update m i val) i = val := begin
+    unfold update get function.update,
+    simp,
+end
+
+lemma get_update_skip (h : i' ≠ i) : get (update m i val) i' = get m i' := begin
+    unfold update get function.update,
+    simp [h],
+end
+
+end memory
+end parlang
