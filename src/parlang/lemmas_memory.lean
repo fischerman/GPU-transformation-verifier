cd26c5f76258cef221b21f7b60765a7b8be9d411
fischerman <code@bjorn-fischer.de>
2019-09-14 11:04:27 +0200

    implement update_update_eq

--- src/parlang/lemmas_memory.lean
+++ src/parlang/lemmas_memory.lean
@@ -3,7 +3,7 @@ import parlang.defs
 namespace parlang
 namespace memory
 
-variables {ι : Type} {τ : ι → Type} [decidable_eq ι] {m : memory τ} {i i' : ι} {val : τ i}
+variables {ι : Type} {τ : ι → Type} [decidable_eq ι] {m : memory τ} {i i' : ι} {val val' : τ i}
 
 lemma get_update_success : get (update m i val) i = val := begin
     unfold update get function.update,
@@ -13,6 +13,13 @@ end
 lemma get_update_skip (h : i' ≠ i) : get (update m i val) i' = get m i' := begin
     unfold update get function.update,
     simp [h],
+end
+
+lemma update_update_eq : update (update m i val) i val' = update m i val' := begin
+    funext i',
+    by_cases h : i' = i,
+    simp [update, function.update, h],
+    simp [update, function.update, h],
 end
 
 end memory
