064a1c3150938578170e906d9d63ebcf81459eb4
fischerman <code@bjorn-fischer.de>
2019-09-19 16:03:56 +0200

    clarify definition of syncablep

--- src/syncablep.lean
+++ src/syncablep.lean
@@ -9,17 +9,13 @@ variables {n : ℕ} {σ : Type} {ι : Type} {τ : ι → Type} [decidable_eq ι]
 /-- syncable with the exception that no thread is allowed to store 
 or load in stores and loads respectively. Adding to stores or load makes this property stricter -/
 def syncable' (shole : set ι) (lhole : set ι) (s : state n σ τ) (m : memory τ) : Prop :=
-(∀i:ι,
-    (∀ tid, i ∉ (s.threads.nth tid).stores ∧ m i = (s.threads.nth tid).shared i) ∨
-    (∃ tid, i ∈ (s.threads.nth tid).stores ∧ m i = (s.threads.nth tid).shared i ∧
-        (∀ tid', tid ≠ tid' → i ∉ (s.threads.nth tid').accesses))) ∧
+state.syncable s m ∧
 (∀ i tid, (i ∈ shole → i ∉ (s.threads.nth tid).stores) ∧
         (i ∈ lhole → i ∉ (s.threads.nth tid).loads))
 
 lemma syncable_syncable' (s : state n σ τ) (m : memory τ) : syncable' ∅ ∅ s m ↔ state.syncable s m := begin
     unfold syncable',
     simp,
-    refl,
 end
 
 
