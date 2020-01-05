25c6d01982197cdf6221ec26f2da029be4f78a7c
Personal <personal@falcon.localdomain>
2020-01-05 20:55:37 +0100

    prove thread_state lemmas

--- src/parlang/lemmas_thread_state.lean
+++ src/parlang/lemmas_thread_state.lean
@@ -6,9 +6,21 @@ variables {n : ℕ} {σ : Type} {ι : Type} {τ : ι → Type} [decidable_eq ι]
 
 
 
-lemma store_accesses {ts : thread_state σ τ} {i} : i ∉ accesses ts → i ∉ stores ts := sorry
+lemma store_accesses {ts : thread_state σ τ} {i} : i ∉ accesses ts → i ∉ stores ts := begin
+  unfold accesses,
+  rw [set.mem_union, not_or_distrib],
+  intros h,
+  cases h,
+  trivial,
+end
 
-lemma loads_accesses {ts : thread_state σ τ} {i} : i ∉ accesses ts → i ∉ loads ts := sorry
+lemma loads_accesses {ts : thread_state σ τ} {i} : i ∉ accesses ts → i ∉ loads ts := begin
+  unfold accesses,
+  rw [set.mem_union, not_or_distrib],
+  intro h,
+  cases h,
+  trivial,
+end
 
 @[simp]
 lemma compute_id : @thread_state.compute σ _ τ _ id = id := begin
@@ -17,11 +29,33 @@ lemma compute_id : @thread_state.compute σ _ τ _ id = id := begin
   simp [thread_state.compute],
 end
 
+#print store._match_1
+
+lemma store_intro (f : σ → (Σi:ι, τ i)) (t : thread_state σ τ) : store f t = { shared := t.shared.update (f t.tlocal).1 (f t.tlocal).2,
+  stores := insert (f t.tlocal).1 t.stores,
+  .. t} := begin
+  sorry,
+end
+
 /-- TODO: rename to store_compute_comm -/
-lemma thread_state_map {f : thread_state σ τ → thread_state σ τ} {g h} : f ∘ thread_state.store g ∘ thread_state.compute h = f ∘ thread_state.compute h ∘ thread_state.store (λ s, g (h s)) := by sorry
+lemma thread_state_map {f : thread_state σ τ → thread_state σ τ} {g h} : f ∘ thread_state.store g ∘ thread_state.compute h = f ∘ thread_state.compute h ∘ thread_state.store (λ s, g (h s)) := begin
+  funext,
+  simp,
+  unfold compute,
+  cases x,
+  rw store_intro,
+  rw store_intro,
+end
 
 /-- TODO: rename to store_compute_comm' -/
-lemma thread_state_map' {g : σ → Σ (i : ι), τ i} {h} : thread_state.store g ∘ thread_state.compute h = thread_state.compute h ∘ thread_state.store (λ s, g (h s)) := by sorry
+lemma thread_state_map' {g : σ → Σ (i : ι), τ i} {h} : thread_state.store g ∘ thread_state.compute h = thread_state.compute h ∘ thread_state.store (λ s, g (h s)) := begin
+  funext,
+  simp,
+  unfold compute,
+  cases x,
+  rw store_intro,
+  rw store_intro,
+end
 
 end thread_state
 end parlang
